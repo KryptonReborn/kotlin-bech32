e1e44fcfa646afa6105726c2d2f35745a69307b2
itanchi.dev <itanchi.dev@gmail.com>
2024-05-04 12:34:26 +0700

    Fixed build related to kotlinx-resources

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,3 +1,8 @@
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnLockMismatchReport
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
+
 plugins {
     id(libs.plugins.commonMppLib.get().pluginId)
 }
@@ -10,9 +15,15 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation(libs.kotlinStdLib)
                 implementation(libs.kotlinxIo)
             }
         }
+    }
+}
+
+rootProject.plugins.withType<YarnPlugin> {
+    rootProject.configure<YarnRootExtension> {
+        yarnLockMismatchReport = YarnLockMismatchReport.WARNING
+        yarnLockAutoReplace = true
     }
 }
