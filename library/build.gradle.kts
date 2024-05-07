9ce577e0281de7e85cebb1a7e2e417d78032a476
BaKarot <doduchieu.kstn@gmail.com>
2024-05-07 21:02:49 +0700

    Align with template (#4)

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,6 +1,20 @@
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnLockMismatchReport
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
+
 plugins {
     id(libs.plugins.commonMppLib.get().pluginId)
+    id(libs.plugins.commonMppPublish.get().pluginId)
 }
+
+publishConfig {
+    url = "https://maven.pkg.github.com/KryptonReborn/kotlin-bech32"
+    groupId = "dev.kryptonreborn.bech32"
+    artifactId = "bech32"
+}
+
+version = "0.0.2"
 
 android {
     namespace = "dev.kryptonreborn.bech32"
@@ -10,9 +24,15 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation(libs.kotlinStdLib)
                 implementation(libs.kotlinxIo)
             }
         }
+    }
+}
+
+rootProject.plugins.withType<YarnPlugin> {
+    rootProject.configure<YarnRootExtension> {
+        yarnLockMismatchReport = YarnLockMismatchReport.WARNING
+        yarnLockAutoReplace = true
     }
 }
